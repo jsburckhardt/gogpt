c8a73d708f525e914133c940f4439707157faf05
juan <jsburckhardt@gmail.com>
2023-05-03 11:58:22 +0000

    chore(install): remove commented code

--- build/install.sh
+++ build/install.sh
@@ -32,11 +32,6 @@ Darwin) OS="osx" ;;
 	;;
 esac
 
-# if [ "$(id -u)" -ne 0 ]; then
-# 	echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
-# 	exit 1
-# fi
-
 # runs the given command as root (detects if we are root already)
 runAsRoot() {
 	local CMD="$*"
